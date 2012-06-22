2963c9bf28c5a97432c6e1469b53b366a5965d09
p12 <tir5c3@yahoo.co.uk>
2012-06-23 02:12:30 +0300

    Add doc-base support

---
+++ debian/doc-base.en.c
@@ -0,0 +1,10 @@
+Document: cppreference-doc-en-c
+Title: C Standard Library reference
+Author: Cppreference.com contributors
+Abstract: A version of online C and C++ standard library reference manual
+ available at en.cppreference.com, suitable for viewing offline.
+Section: Programming/C
+
+Format: HTML
+Index: /usr/share/cppreference/doc/html/en/c.html
+Files: /usr/share/cppreference/doc/html/en/c/*
